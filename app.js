6c5d5d5016ed19608382553cbe9f8c9fbd5c4bc2
Tyler Gaugler <tylergaugler16@gmail.com>
2017-01-22 13:39:17 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var query       = require('./query');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//to serve up static resources.(etc. js/~.js)
+app.use(express.static("/"));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/css", express.static(__dirname + '/css'));
+
+
+app.get("/blog", function(req, res){
+    query('select * from posts', function(err, result){
+      res.render('blog', {posts: result.rows} );
+    });
+});
+app.get("/blog/:id", function(req, res){
+  query('select * from posts where id = $1', [req.params.id], function(err,result){
+      res.render('post', {post: result.rows[0]});
+  });
+});
+app.get("/blog/delete/:id", function(req, res){
+  query('delete from posts where id = $1',[req.params.id], function(err, result){
+      res.redirect("/blog");
+  });
+});
+app.get("/blog/create", function(req, res){
+  res.render("newPost");
+});
+app.post("/blog/create", function(req, res){
+  console.log(req.body.title);
+    query(`insert into posts(title, body) values('${req.body.title}', '${req.body.body}')`, function(err, result){
+      res.redirect("/blog");
+    });
+});
+app.get("/", function(req, res){
+  res.render("portfolio");
+});
+
+app.listen(3000, function(){
+  console.log("yeeeet");
+});
---
+++ query.js
@@ -0,0 +1,34 @@
+var pg = require('pg');
+var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
+
+module.exports = function(queryString, queryParameters, onComplete) {
+  if (typeof queryParameters == 'function') {
+    onComplete = queryParameters;
+    queryParameters = [];
+  }
+
+   pg.connect(connectionString, function(err, client, done) {
+     if (err) {
+       console.log(`error: connection to database failed. connection string: "${connectionString}" ${err}`);
+        if (client) {
+           done(client);
+           }
+        if (onComplete) {
+           onComplete(err);
+         }
+         return;
+       }
+       client.query(queryString, queryParameters, function(err, result) {
+          if (err) {
+             done(client);
+             console.log(`error: query failed: "${queryString}", "${queryParameters}", ${err}`);
+              }
+            else{
+                done();
+              }
+           if (onComplete) {
+             onComplete(err, result);
+           }
+         });
+       });
+     };
